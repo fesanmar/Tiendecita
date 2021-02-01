ca92bb5cd3eaf85ab3450685965cd7fe00120944
Felipe <fesanmar@gmail.com>
2021-02-01 16:36:43 +0100

    Documents exceptions package

--- Tiendecita/src/main/java/dev/felipesantacruz/tiendecita/exceptions/ReportConstructionException.java
+++ Tiendecita/src/main/java/dev/felipesantacruz/tiendecita/exceptions/ReportConstructionException.java
@@ -1,9 +1,19 @@
 package dev.felipesantacruz.tiendecita.exceptions;
 
+/**
+ * Representa una excepción producida durante la creación de un informe.
+ * @author Felipe Santa-Cruz
+ *
+ */
 public class ReportConstructionException extends RuntimeException
 {
 	private static final long serialVersionUID = 1L;
 	
+	
+	/**
+	 * Crear una excepción de construcción de informe.
+	 * @param arg el mensaje que será mostrado en el cuerpo de la excepción.
+	 */
 	public ReportConstructionException(String arg)
 	{
 		super(arg);
